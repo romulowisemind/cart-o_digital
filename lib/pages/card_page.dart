e228a7b1d1400c72131ce8dc3006b915d32c97c2
Romulo Costa <romulocs38@gmail.com>
2022-08-30 09:52:31 -0300

    Adicionado SingleChildScrollView.

--- lib/pages/card_page.dart
+++ lib/pages/card_page.dart
@@ -19,85 +19,84 @@ class CartaoPage extends StatelessWidget {
           'Cartão Digital ',
         ),
       ),
-      body: Column(
-        children: [
-          const Padding(
-            padding: const EdgeInsets.fromLTRB(100, 80, 100, 0),
-            child: CircleAvatar(
-              radius: 130.0,
-              backgroundImage: AssetImage('assets/images/image_card_page.jpeg'),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Padding(
+              padding: const EdgeInsets.fromLTRB(100, 80, 100, 0),
+              child: CircleAvatar(
+                radius: 130.0,
+                backgroundImage: AssetImage('assets/images/image_card_page.jpeg'),
+              ),
             ),
-          ),
-          Text(
-            'Romulo Costa Segato',
-            style: textoStyle,
-          ),
-          Text(
-            'Desenvolvedor Flutter',
-            style: textoStyle1,
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              GestureDetector(
-                onTap: () {
-                  cardController.launchInBrowser(
-                    Uri.parse(
-                        'https://www.linkedin.com/in/romulo-costa-segato-129767178/'),
-                  );
-                },
-                child: Image.asset(
-                  'assets/images/icons/icone_linkedin.png',
-                  width: 40,
-                  height: 40,
+            Text(
+              'Romulo Costa Segato',
+              style: textoStyle,
+            ),
+            Text(
+              'Desenvolvedor Flutter',
+              style: textoStyle1,
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    cardController.launchInBrowser(
+                      Uri.parse(
+                          'https://www.linkedin.com/in/romulo-costa-segato-129767178/'),
+                    );
+                  },
+                  child: Image.asset(
+                    'assets/images/icons/icone_linkedin.png',
+                    width: 40,
+                    height: 40,
+                  ),
                 ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  cardController.launchInBrowser(
-                    Uri.parse('https://github.com/romulowisemind'),
-                  );
-                },
-                child: Image.asset(
-                  'assets/images/icons/icone_github.png',
-                  width: 40,
-                  height: 40,
+                GestureDetector(
+                  onTap: () {
+                    cardController.launchInBrowser(
+                      Uri.parse('https://github.com/romulowisemind'),
+                    );
+                  },
+                  child: Image.asset(
+                    'assets/images/icons/icone_github.png',
+                    width: 40,
+                    height: 40,
+                  ),
                 ),
-              ),
-
-              //usar estrutura de cima
-              GestureDetector(
-                onTap: () {
-                  cardController.launchInBrowser(
-                    Uri.parse(
-                        'https://api.whatsapp.com/send?phone=5534996528235'),
-                  );
-                },
-                child: Image.asset(
-                  'assets/images/icons/icone_whatsapp.png',
-                  width: 40,
-                  height: 40,
+                GestureDetector(
+                  onTap: () {
+                    cardController.launchInBrowser(
+                      Uri.parse(
+                          'https://api.whatsapp.com/send?phone=5534996528235'),
+                    );
+                  },
+                  child: Image.asset(
+                    'assets/images/icons/icone_whatsapp.png',
+                    width: 40,
+                    height: 40,
+                  ),
                 ),
-              ),
-              //usar estrutura de cima
-              GestureDetector(
-                onTap: () {
-                  cardController.launchInBrowser(
-                    Uri.parse('mailto:romulocs38@gmail.com'),
-                  );
-                },
-                child: Image.asset(
-                  'assets/images/icons/icone_email.png',
-                  width: 60,
-                  height: 60,
+                GestureDetector(
+                  onTap: () {
+                    cardController.launchInBrowser(
+                      Uri.parse('mailto:romulocs38@gmail.com'),
+                    );
+                  },
+                  child: Image.asset(
+                    'assets/images/icons/icone_email.png',
+                    width: 60,
+                    height: 60,
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
